3f6641e5d115e07aa42c3ed21407981d85bae189
tk103331 <tk103331@gmail.com>
2019-06-09 23:03:44 +0800

    syntax theme

--- lib/page/code.dart
+++ lib/page/code.dart
@@ -12,6 +12,8 @@ class _CodeViewPageState extends State<CodeViewPage> {
   GitHubFile _file;
   String _name = "";
   String _text = "";
+  Syntax _syntax = Syntax.DART;
+  SyntaxTheme _syntaxTheme = SyntaxTheme.standard();
   @override
   void didChangeDependencies() {
     super.didChangeDependencies();
@@ -29,9 +31,39 @@ class _CodeViewPageState extends State<CodeViewPage> {
       if (contents.isFile) {
         setState(() {
           _text = contents.file.text;
+          _syntax = mappingSyntax(contents.file.type);
         });
       }
     }
+  }
+
+  Syntax mappingSyntax(String type) {
+    switch(type.toLowerCase()) {
+      case "java":
+        return Syntax.JAVA;
+      case "javascript":
+        return Syntax.JAVASCRIPT;
+      case "kotlin":
+        return Syntax.KOTLIN;
+      case "swift":
+        return Syntax.SWIFT;
+      default:
+        return Syntax.DART;
+    }
+  }
+
+  List<PopupMenuItem<SyntaxTheme>> _syntaxThemes() {
+    return <PopupMenuItem<SyntaxTheme>>[
+      PopupMenuItem(value: SyntaxTheme.standard(),child: Text("Standard"),),
+      PopupMenuItem(value: SyntaxTheme.ayuDark(),child: Text("Ayu Dark"),),
+      PopupMenuItem(value: SyntaxTheme.ayuLight(),child: Text("Ayu Light"),),
+      PopupMenuItem(value: SyntaxTheme.dracula(),child: Text("Dracula"),),
+      PopupMenuItem(value: SyntaxTheme.gravityDark(),child: Text("Dravity Dark"),),
+      PopupMenuItem(value: SyntaxTheme.gravityLight(),child: Text("Gravity Light"),),
+      PopupMenuItem(value: SyntaxTheme.monokaiSublime(),child: Text("Monokai Sublime"),),
+      PopupMenuItem(value: SyntaxTheme.obsidian(),child: Text("Obsidian"),),
+      PopupMenuItem(value: SyntaxTheme.oceanSunset(),child: Text("Ocean Sunset"),),
+    ];
   }
 
   @override
@@ -39,12 +71,24 @@ class _CodeViewPageState extends State<CodeViewPage> {
     return Scaffold(
         appBar: AppBar(
           title: Text(_name ?? ""),
+          actions: <Widget>[
+            PopupMenuButton<SyntaxTheme>(
+              onSelected: (SyntaxTheme theme) {
+                setState(() {
+                  _syntaxTheme = theme;
+                });
+              },
+              itemBuilder: (BuildContext context) {
+                return _syntaxThemes();
+              },
+            )
+          ],
         ),
         body: SyntaxView(
           code: _text ?? "",
           withZoom: true,
-          syntax: Syntax.DART,
-          syntaxTheme: SyntaxTheme.dracula(),
+          syntax: _syntax,
+          syntaxTheme: _syntaxTheme,
         ));
   }
 }
