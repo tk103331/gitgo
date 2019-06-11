92749c7b43d90dc69df09d8b0811fc041d43e5bf
tk103331 <tk103331@gmail.com>
2019-06-11 23:39:11 +0800

    dio client

--- lib/api/base.dart
+++ lib/api/base.dart
@@ -1,3 +1,13 @@
 import 'package:github/server.dart';
+import 'package:dio/dio.dart';
 
 GitHub defaultClient = createGitHubClient();
+
+class Client {
+  Dio _dio = new Dio();
+  
+  Future<String> get(String path) async{
+    var resp = await _dio.get(path);
+    return resp.data.toString();
+  }
+}
